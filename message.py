8059c763d06e08c48cd2685af0621adf0d509557
HyeonTae <dchs401@naver.com>
2017-04-29 15:04:38 +0900

    Create message.py

---
+++ message.py
@@ -0,0 +1,47 @@
+REQ_FILE_SEND = 0x01
+REP_FILE_SEND = 0x02
+FILE_SEND_DATA = 0x03
+FILE_SEND_RES = 0x04
+ 
+NOT_FRAGMENTED = 0x00
+FRAGMENTED = 0x01
+ 
+NOT_LASTMSG = 0x00
+LASTMSG = 0x01
+ 
+ACCEPTED = 0x00
+DENIED = 0x01
+ 
+FAIL = 0x00
+SUCCESS = 0x01
+ 
+ 
+class ISerializable:
+    
+    #데이터를 바이트 배열로 변환
+    def GetBytes(self):
+        pass
+ 
+    #바이트 크기 반환
+    def GetSize():
+        pass
+ 
+ 
+ 
+class Message(ISerializable):
+ 
+    #초기화
+    def __init__(self):
+        self.Header = ISerializable()
+        self.Body = ISerializable()
+ 
+    def GetBytes(self):
+        buffer = bytes(self.GetSize())
+ 
+        header = self.Header.GetBytes()
+        body = self.Body.GetBytes()
+ 
+        return header+body
+ 
+    def GetSize(self):
+        return self.Header.GetSize() + self.Body.GetSize()
