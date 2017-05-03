b89f9271c578a10b3e05a289acf802271c50d1da
HyeonTae <dchs401@naver.com>
2017-05-03 19:31:41 +0900

    Create testt.py

---
+++ testt.py
@@ -0,0 +1,17 @@
+from picamera import PiCamera
+from time import sleep
+from gpiozero import Button
+
+button = Button(21)
+stop = 0
+
+while True :
+    stop += 1
+    if(stop >= 6) :
+        break
+    with PiCamera() as camera:
+        camera.rotation = 180
+        camera.start_preview(fullscreen=False, window=(100,20,640,480))
+        button.wait_for_press()
+        camera.capture('image.jpg')
+        camera.stop_preview()
